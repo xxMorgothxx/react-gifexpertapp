5150f6d379c50b32c607833dbf8617418a283ca7
boriscruz <boris.15041@gmail.com>
2020-08-03 20:57:33 -0300

    add docs

---
+++ docs/precache-manifest.df637daa2298d7ffe5148390afefe4a9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "69e2c49ec5699a5f699323df502a0e41",
+    "url": "/index.html"
+  },
+  {
+    "revision": "50052f7eac02ad50d441",
+    "url": "/static/css/main.cf2a8b03.chunk.css"
+  },
+  {
+    "revision": "51858a585169eeb49f95",
+    "url": "/static/js/2.56c56de3.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.56c56de3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "50052f7eac02ad50d441",
+    "url": "/static/js/main.b522799e.chunk.js"
+  },
+  {
+    "revision": "daee87f482498cdf724c",
+    "url": "/static/js/runtime-main.29242b1d.js"
+  }
+]);
